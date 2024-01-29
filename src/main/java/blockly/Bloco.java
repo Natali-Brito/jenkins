e5786a344ec71323d027d6c230f86df5c399226b
Natali Amaral <natali.amaral@cronapp.io>
2024-01-29 19:32:57 +0000

    blocky

--- src/main/java/blockly/Bloco.java
+++ src/main/java/blockly/Bloco.java
@@ -3,8 +3,10 @@ package blockly;
 import cronapi.*;
 import cronapi.rest.security.CronappSecurity;
 import java.util.concurrent.Callable;
+import org.springframework.web.bind.annotation.*;
 
 
+@RestController
 @CronapiMetaData(type = "blockly")
 @CronappSecurity
 public class Bloco {
@@ -16,9 +18,10 @@ public static final int TIMEOUT = 300;
  * @param x
  *
  * @author Natali Amaral
- * @since 18/01/2024, 11:55:24
+ * @since 29/01/2024, 16:30:39
  *
  */
+@RequestMapping(path = { "/api/cronapi/rest/Bloco:Executar", "/Executar" }, method = RequestMethod.GET, consumes = "*/*")
 public static Var Executar(@ParamMetaData(description = "x", id = "993cfd21") Var x) throws Exception {
  return new Callable<Var>() {
 
@@ -36,6 +39,25 @@ public static Var Executar(@ParamMetaData(description = "x", id = "993cfd21") Va
     cronapi.list.Operations.getTextFromList(item,
     Var.valueOf(",")));
     return Var.VAR_NULL;
+   }
+ }.call();
+}
+
+/**
+ *
+ * @author Natali Amaral
+ * @since 29/01/2024, 16:30:39
+ *
+ */
+public static void fa_C3_A7a_algo() throws Exception {
+  new Callable<Var>() {
+
+   public Var call() throws Exception {
+    cronapi.util.Operations.callClientFunction( Var.valueOf("cronapi.screen.notify"), Var.valueOf("success"),
+    cronapi.util.Operations.getSystemProfile());
+    System.out.println(
+    cronapi.util.Operations.getSystemProfile().getObjectAsString());
+   return Var.VAR_NULL;
    }
  }.call();
 }
